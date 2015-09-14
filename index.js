04c686cbdcfe48f82ce8d741f4e855c3ad6c3454
Pavel Karoukin <pavel@karoukin.us>
2015-09-13 21:37:21 -0500

    Added logic for getting guid, service initialization.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
     var metrics = this;
 
     this.orig = {};
-    this.enabled = false;
+    this.frames = [];
 
     this.decorateMap = {
       '$rootScope': {
@@ -18,7 +18,6 @@
           var args = Array.prototype.slice.call(arguments);
           args.shift();
           $orig.call(this, args);
-          console.log('$digest running');
         }
       }
     };
@@ -58,6 +57,12 @@
     }
   };
 
+  Metrics.prototype.enabled = false;
+  Metrics.prototype.cookieName = '__ngmguid';
+
+  Metrics.prototype.init = function() {
+  };
+
   Metrics.prototype.enable = function() {
     this.enabled = true;
   };
@@ -66,9 +71,75 @@
     this.enabled = false;
   };
 
-  Metrics.prototype.$get = function() {
-    return this;
+  Metrics.prototype.getGuid = function() {
+    if (this.docCookies.hasItem(this.cookieName)) {
+      return this.docCookies.getItem(this.cookieName);
+    }
+
+    var guid = this.generateGuid();
+    this.docCookies.setItem(this.cookieName, guid, Infinity, '/');
+
+    return guid;
   };
+
+  Metrics.prototype.generateGuid = function() {
+    function s4() {
+      return Math.floor((1 + Math.random()) * 0x10000)
+        .toString(16)
+        .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+  };
+
+  Metrics.prototype.docCookies = {
+    getItem: function (sKey) {
+      if (!sKey) { return null; }
+      return decodeURIComponent(document.cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1")) || null;
+    },
+    setItem: function (sKey, sValue, vEnd, sPath, sDomain, bSecure) {
+      if (!sKey || /^(?:expires|max\-age|path|domain|secure)$/i.test(sKey)) { return false; }
+      var sExpires = "";
+      if (vEnd) {
+        switch (vEnd.constructor) {
+          case Number:
+            sExpires = vEnd === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + vEnd;
+          break;
+          case String:
+            sExpires = "; expires=" + vEnd;
+          break;
+          case Date:
+            sExpires = "; expires=" + vEnd.toUTCString();
+          break;
+        }
+      }
+      document.cookie = encodeURIComponent(sKey) + "=" + encodeURIComponent(sValue) + sExpires + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "") + (bSecure ? "; secure" : "");
+      return true;
+    },
+    removeItem: function (sKey, sPath, sDomain) {
+      if (!this.hasItem(sKey)) { return false; }
+      document.cookie = encodeURIComponent(sKey) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (sDomain ? "; domain=" + sDomain : "") + (sPath ? "; path=" + sPath : "");
+      return true;
+    },
+    hasItem: function (sKey) {
+      if (!sKey) { return false; }
+      return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test(document.cookie);
+    },
+    keys: function () {
+      var aKeys = document.cookie.replace(/((?:^|\s*;)[^\=]+)(?=;|$)|^\s*|\s*(?:\=[^;]*)?(?:\1|$)/g, "").split(/\s*(?:\=[^;]*)?;\s*/);
+      for (var nLen = aKeys.length, nIdx = 0; nIdx < nLen; nIdx++) { aKeys[nIdx] = decodeURIComponent(aKeys[nIdx]); }
+      return aKeys;
+    }
+  };
+
+  Metrics.prototype.$get = ['$parse', '$rootScope', '$route', function($parse, $rootScope, $route) {
+    this.$parse = $parse;
+    this.$rootScope = $rootScope;
+    this.$route = $route;
+
+    this.init();
+
+    return this;
+  }];
 
   var module = angular.module('ngMetrics', []);
 
